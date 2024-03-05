a3a8052f67fcca4c108a5a78ec6887c93a8298ce
Rakesh <134631155+RakeshKolipaka8466@users.noreply.github.com>
2024-03-05 18:57:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,30 @@ const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [newBox,setNewBox] = useState([]);
 
+  const handleWidth = (e)=>{
+    setWidth(e.target.value);
+  };
+   const handleHeight = (e)=>{
+    setHeight(e.target.value);
+  };
+   const handleBackground = (e)=>{
+    setBackgroundColor(e.target.value);
+  };
+   const handleButton = ()=>{
+    if(width && height &&  backgroundColor){
+    const box ={width:`${width}px`,
+                height:`${height}px`,
+                backgroundColor
+                };
+    setNewBox([...newBox,box]);
+    
+    setWidth("");
+    setHeight("");
+    setBackgroundColor("");
+    }
+  };
   return (
     <div id="main">
       <h1>Box Creator</h1>
@@ -13,25 +36,36 @@ const App = () => {
         <label htmlFor="width">Width:</label>
         <input
           id="width"
-          type="number"
+          type="number"onChange={handleWidth} value={width}
         />
       </div>
       <div>
         <label htmlFor="height">Height:</label>
         <input
           id="height"
-          type="number"
+          type="number"onChange={handleHeight} value={height}
         />
       </div>
       <div>
         <label htmlFor="backgroundColor">Background Color:</label>
         <input
           id="backgroundColor"
-          type="text"
+          type="text"onChange={handleBackground} value={backgroundColor}
         />
       </div>
-      <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <button className='create-btn'onClick={handleButton}>Create Box</button>
+      <div id="box-container">
+       {newBox.map((box,index)=>(
+        <div 
+        key={index}
+        style = {{
+        width:box.width,
+        height:box.height,
+        backgroundColor:box.backgroundColor,
+      }}>
+        </div>
+  ))}
+      </div>
     </div>
   );
 };
